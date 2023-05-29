ca3d5cd72a3a4af1444b6cf1e09cd22d2dff4389
Nathan Henrique <134638765+nathanhenriquy@users.noreply.github.com>
2023-05-29 14:26:18 -0300

    Update main.c

--- main.c
+++ main.c
@@ -1,4 +1,3 @@
-//Escreva um programa que simule a compra de um lanche em uma padaria. O programa deve ser executado enquanto o cliente informar o que deseja comprar e a quantidade desejada. A cada novo item inserido na compra, o programa deve mostrar na tela o subtotal a ser pago. Ao final da execução apresente o valor total da compra.
 #include <stdio.h>
 
 void cardapio (){
